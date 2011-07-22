02d1d1ce2665faf414dd14eef9d1097c4dc7efec
Ovilia <zwl.sjtu@gmail.com>
2011-07-22 13:42:19 +0800

    add Menu class, modify Arrow and Score file name to be capital.

--- Pinbob/src/Arrow.cpp
+++ Pinbob/src/Arrow.cpp
@@ -1,18 +1,39 @@
-/*
- * Arrow.cpp
- *
- *  Created on: Jul 19, 2011
- *      Author: yejiabin
- */
-
 #include "Arrow.h"
 
-Arrow::Arrow() {
-	// TODO Auto-generated constructor stub
+Arrow::Arrow() : arrowType(1), startTime(0), duration(0){}
 
+Arrow::Arrow(unsigned char arrowType) : startTime(0), duration(0){
+	this->arrowType = arrowType;
+}
+	
+Arrow::Arrow(unsigned char arrowType, 
+		unsigned int startTime, unsigned int duration){
+	this->arrowType = arrowType;
+	this->startTime = startTime;
+	this->duration = duration;
 }
 
-Arrow::~Arrow() {
-	// TODO Auto-generated destructor stub
+unsigned char Arrow::getArrowType(){
+	return arrowType;
+}
+
+unsigned int Arrow::getStartTime(){
+	return startTime;
+}
+
+unsigned int Arrow::getDuration(){
+	return duration;
+}
+
+void Arrow::setArrowType(unsigned char arrowType){
+	this->arrowType = arrowType;
+}
+
+void Arrow::setStartTime(unsigned int startTime){
+	this->startTime = startTime;
+}
+
+void Arrow::setDuration(unsigned int duration){
+	this->duration = duration;
 }
 
--- Pinbob/src/Arrow.h
+++ Pinbob/src/Arrow.h
@@ -1,17 +1,25 @@
-/*
- * Arrow.h
- *
- *  Created on: Jul 19, 2011
- *      Author: yejiabin
- */
+#ifndef ARROW_H
+#define ARROW_H
 
-#ifndef ARROW_H_
-#define ARROW_H_
-
-class Arrow {
+class Arrow{
 public:
 	Arrow();
-	virtual ~Arrow();
+	Arrow(unsigned char arrowType);
+	Arrow(unsigned char arrowType, 
+		unsigned int startTime, unsigned int duration);
+		
+	unsigned char getArrowType();
+	unsigned int getStartTime();
+	unsigned int getDuration();
+	
+	void setArrowType(unsigned char arrowType);
+	void setStartTime(unsigned int startTime);
+	void setDuration(unsigned int duration);
+
+private:
+	unsigned char arrowType;
+	unsigned int startTime;
+	unsigned int duration;
 };
 
-#endif /* ARROW_H_ */
+#endif
