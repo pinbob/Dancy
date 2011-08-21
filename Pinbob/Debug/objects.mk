22465465b886f3a9ae7c2dac8d60105b4acc2a4b
Ye Jiabin <alpha360x@gmail.com>
2011-08-21 23:09:11 +0800

    stop tracking some binaries and makefile generated by eclispse

--- Pinbob/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lIrrlicht -lvorbisfile -lIrrKlang -lgstreamer-0.10 -lARgsub -lARgsubUtil -lARMulti -lAR -lARvideo
-
