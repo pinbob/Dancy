18fc7dd5fe87286f078ee6bde02d511d29605197
Ye Jiabin <alpha360x@gmail.com>
2011-08-22 15:08:30 +0800

    Revert "stop tracking some binaries and makefile generated by eclispse"
    This reverts commit 22465465b886f3a9ae7c2dac8d60105b4acc2a4b.

---
+++ Pinbob/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lIrrlicht -lvorbisfile -lIrrKlang -lgstreamer-0.10 -lARgsub -lARgsubUtil -lARMulti -lAR -lARvideo
+
