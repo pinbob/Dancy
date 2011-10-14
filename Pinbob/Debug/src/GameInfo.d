58bf90abda39f611bf8ac57e69a17af27d564bb6
Ye Jiabin <alpha360x@gmail.com>
2011-10-15 01:07:26 +0800

    remove some obj file

--- Pinbob/Debug/src/GameInfo.d
+++
@@ -1,6 +0,0 @@
-src/GameInfo.d src/GameInfo.o: ../src/GameInfo.cpp ../src/GameInfo.h \
- ../src/Score.h
-
-../src/GameInfo.h:
-
-../src/Score.h:
--- Pinbob/Debug/src/NoteTypes.d
+++
@@ -1,6 +0,0 @@
-src/NoteTypes.d src/NoteTypes.o: ../src/NoteTypes.cpp \
- ../src/note_config.h ../src/NoteTypes.h
-
-../src/note_config.h:
-
-../src/NoteTypes.h:
